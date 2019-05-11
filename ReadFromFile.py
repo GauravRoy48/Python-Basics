3af381e4b1ff3e51a58be11961dd205c49497369
Gaurav Roy <gauravroy0408@gmail.com>
2019-05-11 11:25:44 +0530

    Using "with" context manager for optimization
    Using "with" while handling files so that at the end of the block, the file closes on its own.
    If the older method is used and the code breaks before myfile.close() is executed, then then the file isn't closed properly.
    With the newer method, the block of code automatically closes the file as soon as execution is over or if the block breaks.

--- ReadFromFile.py
+++ ReadFromFile.py
@@ -1,17 +1,35 @@
-myfile = open("fruits.txt")
-fruits_read = myfile.read()
-myfile.seek(0)
-fruits_readline = myfile.readline()
-myfile.seek(0)
-fruits_readlines = myfile.readlines()
-fruits_readlines_stripped = [line.strip() for line in fruits_readlines]
+#-----------------------------------------BEFORE------------------------------------------------------
+#myfile = open("fruits.txt")
+#fruits_read = myfile.read()
+#myfile.seek(0)
+#fruits_readline = myfile.readline()
+#myfile.seek(0)
+#fruits_readlines = myfile.readlines()
+#fruits_readlines_stripped = [line.strip() for line in fruits_readlines]
+#
+#print(fruits_read)
+#myfile.close()
+#-----------------------------------------AFTER------------------------------------------------------
+with open("fruits.txt") as myfile:
+    fruits_read = myfile.read()
+    myfile.seek(0)
+    fruits_readline = myfile.readline()
+    myfile.seek(0)
+    fruits_readlines = myfile.readlines()
+    fruits_readlines_stripped = [line.strip() for line in fruits_readlines]
 
-print(fruits_read)
-myfile.close()
+    print(fruits_read)
 
-myfile = open("numbers.txt","w")
-numbers = [1,2,3]
-for i in numbers:
-    myfile.write(str(i)+'\n')
-    
-myfile.close()
+
+#-----------------------------------------BEFORE------------------------------------------------------
+#myfile = open("numbers.txt","w")
+#numbers = [1,2,3]
+#for i in numbers:
+#    myfile.write(str(i)+'\n')
+#
+#myfile.close()
+#-----------------------------------------AFTER------------------------------------------------------
+with open("numbers.txt","w") as myfile:
+    numbers = [1,2,3]
+    for i in numbers:
+        myfile.write(str(i)+'\n')
